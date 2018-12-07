ecfdeb67cb27dd53489e0944192209675cbf1f5c
wanxujie <wbb21@vip.qq.com>
2018-12-07 10:31:44 +0800

    1

--- mea.js
+++ mea.js
@@ -39,4 +39,39 @@ function scheduleCronstyle(){
     })
 }
 
+function scheduleCronstyle1(){
+    schedule.scheduleJob('0 0 17 * * *', function(){
+        request('http://api.goseek.cn/Tools/holiday', function (error, response, body) {
+            if (JSON.parse(body).data === 0) {
+                var content = {
+                    "msgtype": "text",
+                    "text": {
+                        "content": '各位研发老C, 龙哥熊妹提醒各位: 17:00, 我们开始剪辑录屏课啦～ 翻牌子今儿到此结束～'
+                    },
+                    "at": {
+                        "isAtAll": true
+                    }
+                }
+                var  options = {
+                    method: 'post',
+                    url: 'https://oapi.dingtalk.com/robot/send?access_token=ca950c0616667abe38a22d9dc063c78cf48235a6b288184e16aa7afd92894761',
+                    json: content,
+                    headers: {
+                        'Content-Type': 'application/json;charset=utf-8'
+                    }
+                };
+                request(options, function (err, res, body1) {
+                    console.log(options)
+                    if (err) {
+                        console.log('err', err)
+                    }else {
+                        console.log('body1', body1);
+                    }
+                })
+            }
+        })
+    })
+}
+
 scheduleCronstyle();
+scheduleCronstyle1();
